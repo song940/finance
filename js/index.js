a64178f05b1e8542443a82d7abef1e94391f287e
Lsong <song940@gmail.com>
2022-12-21 22:58:33 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,288 +1,198 @@
 import { ready } from 'https://lsong.org/scripts/dom.js';
-import { h, render, useState, useEffect } from 'https://lsong.org/scripts/components/react.js';
+import { format } from 'https://lsong.org/scripts/date.js';
 import * as echarts from 'https://unpkg.com/echarts@5.4.1/dist/echarts.esm.js'
 
-const App = () => {
-  const [] = useState();
-  useEffect(() => {
-    console.log('App is ready');
-  }, []);
-  return [
-    h('h2', null, "App"),
-    h('ul', null, [
-      h('li', null, "Link")
-    ])
-  ]
-};
+// function splitData(rawData) {
+//   let categoryData = [];
+//   let values = [];
+//   let volumes = [];
+//   for (let i = 0; i < rawData.length; i++) {
+//     categoryData.push(rawData[i].splice(0, 1)[0]);
+//     values.push(rawData[i]);
+//     volumes.push([i, rawData[i][4], rawData[i][0] > rawData[i][1] ? 1 : -1]);
+//   }
+//   return {
+//     categoryData: categoryData,
+//     values: values,
+//     volumes: volumes
+//   };
+// }
 
-var chartDom = document.getElementById('app');
-var myChart = echarts.init(chartDom);
-var option;
-
-const upColor = '#00da3c';
-const downColor = '#ec0000';
-function splitData(rawData) {
-  let categoryData = [];
-  let values = [];
-  let volumes = [];
-  for (let i = 0; i < rawData.length; i++) {
-    categoryData.push(rawData[i].splice(0, 1)[0]);
-    values.push(rawData[i]);
-    volumes.push([i, rawData[i][4], rawData[i][0] > rawData[i][1] ? 1 : -1]);
-  }
+const buildOption = data => {
+  const upColor = '#ec0000';
+  const downColor = '#00da3c';
   return {
-    categoryData: categoryData,
-    values: values,
-    volumes: volumes
-  };
-}
-function calculateMA(dayCount, data) {
-  var result = [];
-  for (var i = 0, len = data.values.length; i < len; i++) {
-    if (i < dayCount) {
-      result.push('-');
-      continue;
-    }
-    var sum = 0;
-    for (var j = 0; j < dayCount; j++) {
-      sum += data.values[i - j][1];
-    }
-    result.push(+(sum / dayCount).toFixed(3));
-  }
-  return result;
-}
-
-fetch('data.json').then(res => res.json()).then(function (rawData) {
-  console.log(rawData);
-  var data = splitData(rawData);
-  myChart.setOption(
-    (option = {
-      animation: false,
-      legend: {
-        bottom: 10,
-        left: 'center',
-        data: ['Dow-Jones index', 'MA5', 'MA10', 'MA20', 'MA30']
-      },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'cross'
-        },
-        borderWidth: 1,
-        borderColor: '#ccc',
-        padding: 10,
-        textStyle: {
-          color: '#000'
-        },
-        position: function (pos, params, el, elRect, size) {
-          const obj = {
-            top: 10
-          };
-          obj[['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]] = 30;
-          return obj;
-        }
-        // extraCssText: 'width: 170px'
-      },
+    animation: false,
+    legend: {
+      bottom: 10,
+      left: 'center',
+      data: ['Dow-Jones index']
+    },
+    tooltip: {
+      trigger: 'axis',
       axisPointer: {
-        link: [
-          {
-            xAxisIndex: 'all'
-          }
-        ],
-        label: {
-          backgroundColor: '#777'
-        }
+        type: 'cross'
       },
-      toolbox: {
-        feature: {
-          dataZoom: {
-            yAxisIndex: false
-          },
-          brush: {
-            type: ['lineX', 'clear']
-          }
-        }
+      borderWidth: 1,
+      borderColor: '#ccc',
+      padding: 10,
+      textStyle: {
+        color: '#000'
       },
-      brush: {
-        xAxisIndex: 'all',
-        brushLink: 'all',
-        outOfBrush: {
-          colorAlpha: 0.1
-        }
-      },
-      visualMap: {
-        show: false,
-        seriesIndex: 5,
-        dimension: 2,
-        pieces: [
-          {
-            value: 1,
-            color: downColor
-          },
-          {
-            value: -1,
-            color: upColor
-          }
-        ]
-      },
-      grid: [
+    },
+    axisPointer: {
+      link: [
         {
-          left: '10%',
-          right: '8%',
-          height: '50%'
-        },
-        {
-          left: '10%',
-          right: '8%',
-          top: '63%',
-          height: '16%'
+          xAxisIndex: 'all'
         }
       ],
-      xAxis: [
-        {
-          type: 'category',
-          data: data.categoryData,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          splitLine: { show: false },
-          min: 'dataMin',
-          max: 'dataMax',
-          axisPointer: {
-            z: 100
-          }
+      label: {
+        backgroundColor: '#777'
+      }
+    },
+    toolbox: {
+      feature: {
+        dataZoom: {
+          yAxisIndex: false
         },
-        {
-          type: 'category',
-          gridIndex: 1,
-          data: data.categoryData,
-          boundaryGap: false,
-          axisLine: { onZero: false },
-          axisTick: { show: false },
-          splitLine: { show: false },
-          axisLabel: { show: false },
-          min: 'dataMin',
-          max: 'dataMax'
+        brush: {
+          type: ['lineX', 'clear']
         }
-      ],
-      yAxis: [
+      }
+    },
+    brush: {
+      xAxisIndex: 'all',
+      brushLink: 'all',
+      outOfBrush: {
+        colorAlpha: 0.1
+      }
+    },
+    visualMap: {
+      show: false,
+      seriesIndex: 1,
+      dimension: 2,
+      pieces: [
         {
-          scale: true,
-          splitArea: {
-            show: true
-          }
+          value: 1,
+          color: downColor
         },
         {
-          scale: true,
-          gridIndex: 1,
-          splitNumber: 2,
-          axisLabel: { show: false },
-          axisLine: { show: false },
-          axisTick: { show: false },
-          splitLine: { show: false }
-        }
-      ],
-      dataZoom: [
-        {
-          type: 'inside',
-          xAxisIndex: [0, 1],
-          start: 98,
-          end: 100
-        },
-        {
-          show: true,
-          xAxisIndex: [0, 1],
-          type: 'slider',
-          top: '85%',
-          start: 98,
-          end: 100
-        }
-      ],
-      series: [
-        {
-          name: 'Dow-Jones index',
-          type: 'candlestick',
-          data: data.values,
-          itemStyle: {
-            color: upColor,
-            color0: downColor,
-            borderColor: undefined,
-            borderColor0: undefined
-          },
-          tooltip: {
-            formatter: function (param) {
-              param = param[0];
-              return [
-                'Date: ' + param.name + '<hr size=1 style="margin: 3px 0">',
-                'Open: ' + param.data[0] + '<br/>',
-                'Close: ' + param.data[1] + '<br/>',
-                'Lowest: ' + param.data[2] + '<br/>',
-                'Highest: ' + param.data[3] + '<br/>'
-              ].join('');
-            }
-          }
-        },
-        {
-          name: 'MA5',
-          type: 'line',
-          data: calculateMA(5, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA10',
-          type: 'line',
-          data: calculateMA(10, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA20',
-          type: 'line',
-          data: calculateMA(20, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'MA30',
-          type: 'line',
-          data: calculateMA(30, data),
-          smooth: true,
-          lineStyle: {
-            opacity: 0.5
-          }
-        },
-        {
-          name: 'Volume',
-          type: 'bar',
-          xAxisIndex: 1,
-          yAxisIndex: 1,
-          data: data.volumes
+          value: -1,
+          color: upColor
         }
       ]
-    }),
-    true
-  );
-  myChart.dispatchAction({
-    type: 'brush',
-    areas: [
+    },
+    grid: [
       {
-        brushType: 'lineX',
-        coordRange: ['2016-06-02', '2016-06-20'],
-        xAxisIndex: 0
+        left: '10%',
+        right: '8%',
+        height: '50%'
+      },
+      {
+        left: '10%',
+        right: '8%',
+        top: '63%',
+        height: '16%'
+      }
+    ],
+    xAxis: [
+      {
+        type: 'category',
+        data: data.categories,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        splitLine: { show: false },
+        min: 'dataMin',
+        max: 'dataMax',
+        axisPointer: {
+          z: 100
+        }
+      },
+      {
+        type: 'category',
+        gridIndex: 1,
+        data: data.categoryData,
+        boundaryGap: false,
+        axisLine: { onZero: false },
+        axisTick: { show: false },
+        splitLine: { show: false },
+        axisLabel: { show: false },
+        min: 'dataMin',
+        max: 'dataMax'
+      }
+    ],
+    yAxis: [
+      {
+        scale: true,
+        splitArea: {
+          show: true
+        }
+      },
+      {
+        scale: true,
+        gridIndex: 1,
+        splitNumber: 2,
+        axisLabel: { show: false },
+        axisLine: { show: false },
+        axisTick: { show: false },
+        splitLine: { show: false }
+      }
+    ],
+    dataZoom: [
+      {
+        type: 'inside',
+        xAxisIndex: [0, 1],
+        start: 0,
+        end: 100
+      },
+      {
+        show: true,
+        xAxisIndex: [0, 1],
+        type: 'slider',
+        top: '80%',
+        start: 0,
+        end: 100
+      }
+    ],
+    series: [
+      {
+        name: 'Dow-Jones index',
+        type: 'candlestick',
+        data: data.values,
+        itemStyle: {
+          color: upColor,
+          color0: downColor,
+          borderColor: undefined,
+          borderColor0: undefined
+        },
+      },
+      {
+        name: 'Volume',
+        type: 'bar',
+        xAxisIndex: 1,
+        yAxisIndex: 1,
+        data: data.volumes
       }
     ]
-  });
-});
+  };
+};
 
-option && myChart.setOption(option);
+const fetchData = async () => {
+  const res = await fetch(`https://api.huobi.pro/market/history/kline?period=1day&size=200&symbol=btcusdt`)
+  const data = await res.json();
+  const categories = [], values = [], volumes = [];
+  for (const row of data.data.sort((a, b) => a.id - b.id)) {
+    categories.push(format('{yyyy}-{MM}-{dd}', new Date(row.id * 1000)));
+    volumes.push([row.vol, row.vol, row.open > row.close ? 1 : -1]);
+    values.push([row.open, row.close, row.low, row.high]);
+  }
+  return { categories, values, volumes };
+};
 
-ready(() => {
-  // const app = document.getElementById('app');
-  // render(h(App), app);
+ready(async () => {
+  const app = document.getElementById('app');
+  const chart = echarts.init(app);
+  const data = await fetchData();
+  const option = buildOption(data);
+  chart.setOption(option, true);
 });
